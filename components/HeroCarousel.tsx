1a2ff511f989d0ef183ee47c39135acbb67c5e8f
chaimyaham <chaim.yaham17@gmail.com>
2023-10-18 21:45:18 +0100

    scraping setup

--- components/HeroCarousel.tsx
+++ components/HeroCarousel.tsx
@@ -14,14 +14,17 @@ const heroImages =[
 const HeroCarousel = () => {
   return (
     <div className="hero-carousel">
-         <Carousel showThumbs={false} autoPlay infiniteLoop interval={2000}
+         <Carousel showThumbs={false}
+          // autoPlay infiniteLoop 
+          // interval={2000}
          showArrows={false} showStatus={false}>
          {
             heroImages.map((image)=>(
-                <Image alt={image.alt} key={image.alt} src={image.imgUrl} width={450} height={450} className="object-contain"/>
+                <Image alt={image.alt} key={image.alt} src={image.imgUrl} width={484} height={484} className="object-contain"/>
             ))
          }
          </Carousel>
+         <Image src="assets/icons/hand-drawn-arrow.svg" alt="arrow down " width={175} height={175} className="max-xl:hidden absolute -left-[15%] bottom-0 z-0"/>
     </div>
   )
 }
--- components/SearchBar.tsx
+++ components/SearchBar.tsx
@@ -1,17 +1,71 @@
 "use client"
 
+import { FormEvent, useState } from "react"
+
+const isValidAmazonProductUrl = (url : string)=>{
+  try {
+    const  parsedURL = new URL(url);
+    const hostname= parsedURL.hostname;
+    if(
+      hostname.includes('amazon.com') ||
+       hostname.includes('amazon.') || 
+       hostname.includes('amazon')
+      )
+       {
+        return true;
+
+    }
+    
+  } catch (error) {
+    return false;
+    
+  }
+
+  return false;
+
+
+}
+
 const SearchBar = () => {
-    const handleSubmit=()=>{
+  const [searchPrompt, setsearchPrompt] = useState('');
+  const [isLoading, setIsLoading] = useState(false);
+    const handleSubmit=(event: FormEvent<HTMLFormElement>)=>{
+
+      event.preventDefault();
+      const isValidLink = isValidAmazonProductUrl(searchPrompt);
+      if(!isValidLink) return alert('please enter a valid amazon link');
+
+      try {
+        setIsLoading(true);
+
+        // Scrape the product page
+        
+
+        
+      } catch (error) {
+        console.log(error);
+
+        
+      }finally{
+
+        setIsLoading(false);
+      }
+      
 
     }
   return (
     <form className='flex flex-wrap gap-4 mt-12' onSubmit={handleSubmit}>
 
         <input type="text"
+        value={searchPrompt}
+        onChange={(e)=>setsearchPrompt(e.target.value)}
         placeholder="Enter Product Link" 
         className="searchbar-input"/>
-        <button type="submit" className="searchbar-btn">
-            Search
+        <button type="submit" 
+        className="searchbar-btn"
+        disabled = {searchPrompt ===''}
+        >
+           {isLoading ? 'searching' : 'Search'}
         </button>
 
     </form>
